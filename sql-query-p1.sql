c022be80d94ca62cf9f1a7531f9c87b14c660104
Pusp203 <58679122+Pusp203@users.noreply.github.com>
2026-01-18 08:46:28 +0545

    Add files via upload

---
+++ sql-query-p1.sql
@@ -0,0 +1,184 @@
+-- SQL Retail Sales Analysis--Project 1
+
+-- Creating tables
+	drop table if exists retail_sales;
+
+	create table retail_sales
+	(
+		transactions_id INT PRIMARY KEY,
+		sale_date DATE,
+		sale_time TIME,
+		customer_id int,
+		gender varchar(15),
+		age int,
+		category varchar(15) ,
+		quantiy int,
+		price_per_unit float,
+		cogs float,
+		total_sale float
+	)
+
+
+
+	
+	select count(*) from retail_sales;
+
+-- Checking null values // Data Cleaning
+	select * from retail_sales
+	where
+	transactions_id is null
+	or
+	sale_date is null
+	or
+	sale_time is null
+	or
+	gender is null
+	or
+	category is null
+	or
+	quantiy is null
+	or
+	cogs is null
+	or 
+	total_sale is null
+	;
+	
+-- Removing Duplicates
+delete from retail_sales
+where
+	transactions_id is null
+	or
+	sale_date is null
+	or
+	sale_time is null
+	or
+	gender is null
+	or
+	category is null
+	or
+	quantiy is null
+	or
+	cogs is null
+	or 
+	total_sale is null;
+
+-- Data Exploration
+--How many sales we have>?
+select count(*) as total_sale from retail_sales;
+
+--How many unique customers we have??
+
+select count( distinct customer_id) as unique_customers from retail_sales;
+
+--How many categories we have?
+
+select distinct category from retail_sales;
+
+-- Data Analysis and Business Analysis key problems and Answers.
+
+-- Q.1 Write a SQL query to retrieve all columns for sales made on '2022-11-05'
+
+	
+	select * from retail_sales
+	where sale_date = '2022-11-05';
+	
+-- Q.2 Write a SQL query to retrieve all transactions where the category is 'Clothing' and the quantity sold is more than 10 in the month of Nov-2022
+
+	select * from retail_sales;
+	
+
+	SELECT 
+  *
+FROM retail_sales
+WHERE 
+    category = 'Clothing'
+    AND 
+    TO_CHAR(sale_date, 'YYYY-MM') = '2022-11'
+    AND
+    quantiy >= 4
+	
+-- Q.3 Write a SQL query to calculate the total sales (total_sale) for each category.
+
+	SELECT 
+	    category,
+	    SUM(total_sale) as net_sale,
+	    COUNT(*) as total_orders
+	FROM retail_sales
+	GROUP BY 1
+
+-- Q.4 Write a SQL query to find the average age of customers who purchased items from the 'Beauty' category.
+			SELECT
+	    ROUND(AVG(age), 2) as avg_age
+	FROM retail_sales
+	WHERE category = 'Beauty'
+
+
+-- Q.5 Write a SQL query to find all transactions where the total_sale is greater than 1000.
+	
+			SELECT * FROM retail_sales
+	WHERE total_sale > 1000;
+
+-- Q.6 Write a SQL query to find the total number of transactions (transaction_id) made by each gender in each category.
+
+		SELECT 
+	    category,
+	    gender,
+	    COUNT(*) as total_trans
+	FROM retail_sales
+	GROUP 
+	    BY 
+	    category,
+	    gender
+	ORDER BY 1;
+
+-- Q.7 Write a SQL query to calculate the average sale for each month. Find out best selling month in each year
+
+		SELECT 
+	       year,
+	       month,
+	    avg_sale
+	FROM 
+	(    
+	SELECT 
+	    EXTRACT(YEAR FROM sale_date) as year,
+	    EXTRACT(MONTH FROM sale_date) as month,
+	    AVG(total_sale) as avg_sale,
+	    RANK() OVER(PARTITION BY EXTRACT(YEAR FROM sale_date) ORDER BY AVG(total_sale) DESC) as rank
+	FROM retail_sales
+	GROUP BY 1, 2
+	) as t1
+	WHERE rank = 1;
+	
+-- Q.8 Write a SQL query to find the top 5 customers based on the highest total sales 
+
+		
+	SELECT 
+	    customer_id,
+	    SUM(total_sale) as total_sales
+	FROM retail_sales
+	GROUP BY 1
+	ORDER BY 2 DESC
+	LIMIT 5
+-- Q.9 Write a SQL query to find the number of unique customers who purchased items from each category.
+
+SELECT 
+    category,    
+    COUNT(DISTINCT customer_id) as cnt_unique_cs
+FROM retail_sales
+GROUP BY category
+
+
+-- Q.10 Write a SQL query to create each shift and number of orders (Example Morning <=12, Afternoon Between 12 & 17, Evening >17)
+
+	WITH hourly_sale
+	AS
+	(
+	SELECT *,
+	    CASE
+	        WHEN EXTRACT(HOUR FROM sale_time) < 12 THEN 'Morning'
+	        WHEN EXTRACT(HOUR FROM sale_time) BETWEEN 12 AND 17 THEN 'Afternoon'
+	        ELSE 'Evening'
+	    END as shift
+	FROM retail_sales
+
+	
